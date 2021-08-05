a83c853cb650b8a9680f7eab9139cbf37ff0e5cd
TsurikViktor <tsurik.viktor.vladimirovich@gmail.com>
2021-08-06 00:27:40 +0300

    commit 1

---
+++ js/customs.js
@@ -0,0 +1,18 @@
+var age = +prompt('Введите ваш возраст');
+var z = confirm('Вы курите ?  Да(ОК)  НЕТ(Отмена)');
+if ( age < 18 ) {
+    if ( z === false ) {
+        alert('Так держать !!!');
+    } 
+    else if ( z === true ) {
+        alert('Маме расскажу !!!');
+    }
+} 
+else if (age => 18){
+    if ( z === true ) {
+        alert('Ну и зря !!!');
+    } 
+    else if ( z === false ) {
+        alert('Молодец, и не надо !!!');
+    }
+}
